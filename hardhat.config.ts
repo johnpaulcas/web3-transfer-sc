25dd969f22d18bbf7f8cfa88a876f23cec2c0baa
johnpaulcas <casjp.app@gmail.com>
2023-05-27 00:12:55 +0800

    🚀 configure zksync deployment to testnet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,10 +4,56 @@ dotenv.config()
 import { HardhatUserConfig } from 'hardhat/config'
 import '@nomicfoundation/hardhat-toolbox'
 
+import '@nomicfoundation/hardhat-foundry'
+
+import '@matterlabs/hardhat-zksync-verify'
+import '@matterlabs/hardhat-zksync-solc'
+
 import 'hardhat-deploy'
 
+const zkTestnet = {
+  url: process.env.ZK_TESTNET_URL || '',
+  ethNetwork: 'goerli',
+  zksync: true,
+  accounts:
+    process.env.ZK_TESTNET_PRIVATE_KEY !== undefined
+      ? [process.env.ZK_TESTNET_PRIVATE_KEY]
+      : [],
+  verifyURL: process.env.ZK_TESTNET || '',
+}
+
 const config: HardhatUserConfig = {
-  solidity: '0.8.18',
+  zksolc: {
+    version: '1.3.8',
+    compilerSource: 'binary',
+    settings: {},
+  },
+  solidity: {
+    version: '0.8.9',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    goerli: {
+      url: process.env.GOERLI_URL || '',
+    },
+    zkTestnet,
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0, // here this will by default take the first account as deployer
+      1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+    },
+  },
+  paths: {
+    sources: 'src',
+    cache: 'cache-zk',
+    artifacts: 'artifacts-zk',
+  },
 }
 
 export default config
