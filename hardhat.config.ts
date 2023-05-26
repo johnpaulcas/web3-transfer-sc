5e946e6e25dfd8265462845c0ddb56c67314504b
johnpaulcas <casjp.app@gmail.com>
2023-05-26 23:48:02 +0800

    🔧 add hardhat-deploy to config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,8 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+import "hardhat-deploy";
+
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
 };
